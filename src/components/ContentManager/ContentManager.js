56ac924276fa299369c44c95637181b276d7307e
Logan <l.karstens.d@gmail.com>
2023-01-18 01:25:05 -0500

    added scroll transition effect

--- src/components/ContentManager/ContentManager.js
+++ src/components/ContentManager/ContentManager.js
@@ -1,4 +1,4 @@
-import { useEffect, useContext, useCallback } from 'react';
+import { useState, useEffect, useContext, useCallback } from 'react';
 import styles from './ContentManager.module.css'
 import BottomText from '../Content/BottomText';
 import Resume from '../Content/Resume';
@@ -6,6 +6,8 @@ import Contact from '../Content/Contact';
 import PageContext from '../../Store/page-context';
 
 const ContentManager = () => {
+    const [moveStyle, setMoveStyle] = useState(0);
+    const [xPosition, setXPosition] = useState("0vh");
     // const [swipeData, setSwipeData] = useState();
     const ctx = useContext(PageContext);
     const scrollHandler = useCallback((e) => {
@@ -13,7 +15,20 @@ const ContentManager = () => {
         if (!(resume && resume.contains(e.target)) && ctx.currentPage === ctx.delayedPage) {
             let index = ctx.pages.findIndex(maybePage => maybePage === ctx.currentPage)
             if((e.deltaY > 0 && index < ctx.pages.length - 1) || (e.deltaY < 0 && index > 0)) {
-                index += e.deltaY > 0 ? 1 : -1;
+                const change = e.deltaY > 0 ? 1 : -1;
+
+                if (change === 1) {
+                    setMoveStyle(styles['effect-bottom']);
+                    setXPosition((Math.random()*200 - 200) + "vw")
+                    setTimeout(setMoveStyle.bind(null, ''), 600);
+
+                } else {
+                    setMoveStyle(styles['effect-top']);
+                    setXPosition((Math.random()*200 - 200) + "vw")
+                    setTimeout(setMoveStyle.bind(null, ''), 600);
+                }
+
+                index += change;
                 ctx.changePage(ctx.pages[index]);
             }
         }
@@ -43,12 +58,14 @@ const ContentManager = () => {
         
     }, [ctx, scrollHandler])
 
+
     const isVerticalLayout = (ctx.delayedPage === "projects" || ctx.delayedPage === "contact")
 
-    
     return (
         <>
             <div id="contentmanager" className={`${styles.container} ${isVerticalLayout && styles.verticalLayout}`} onScroll={scrollHandler}>
+                <div className={`${styles.effect} ${moveStyle}`} style={{left: xPosition}}></div>
+                
                 <div className={styles.filler}>
                 <BottomText />
                 </div>
